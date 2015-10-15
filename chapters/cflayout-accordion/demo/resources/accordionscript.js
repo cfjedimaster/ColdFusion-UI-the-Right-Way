5e300330a689aebee07116db878c7b5f939b149d
Steve Milburn <scmilburn@gmail.com>
2015-10-15 09:48:38 -0400

    Make sure target is defined before loading content.

--- chapters/cflayout-accordion/demo/resources/accordionscript.js
+++ chapters/cflayout-accordion/demo/resources/accordionscript.js
@@ -1,12 +1,11 @@
 $(function() {
-	//initialize the accordion
 	$("#accordion").accordion({
 		beforeActivate: function(event, ui) {
 			var $header = ui.newHeader,
 				content = $header.data("content"),
 				target = $header.data("target");
 
-			if (content !== undefined) {
+			if (content !== undefined && target !== undefined) {
 				$(target).load(content);
 			}
 		}
